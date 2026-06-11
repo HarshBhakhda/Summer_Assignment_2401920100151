a49cf9e5fb02b354520e5340256258843dff4602
HarshBhakhda <harshbhakhda@gmail.com>
2026-06-11 23:47:13 +0530

    Add function to reverse words in a string
    Implemented a function to reverse words in a string.

--- Week_2/Day_4/Reverse Words in String III.cpp
+++ Week_2/Day_4/Reverse Words in String III.cpp
@@ -1,3 +1,22 @@
-// Reverse Words in String III
-// Write your code here
+void reverse(string& s,int i,int j) {
+    while(j>=i) {
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }
+}
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l=0,i=0;
+        for(i=0;i<s.size();i++) {
+            if(s[i]==' ') {
+                reverse(s,l,i-1);
+                l=i+1;
+            }
+        }
+        reverse(s,l,i-1);
+        return s;
+    }
+};
